a4cc477d8a41f2b471567d0582fffa079527d383
Klbg94 <101947640+Klbg94@users.noreply.github.com>
2022-08-06 12:58:18 +0300

    Add files via upload

---
+++ Heart Trail Animation/script.js
@@ -0,0 +1,16 @@
+const bodyEl = document.querySelector("body");
+
+bodyEl.addEventListener("mousemove", (e) => {
+  const xPos = e.offsetX;
+  const yPos = e.offsetY;
+  const spanEl = document.createElement("span");
+  spanEl.style.left = xPos + "px";
+  spanEl.style.top = yPos + "px";
+  const size = Math.random() * 100;
+  spanEl.style.width = size + "px";
+  spanEl.style.height = size + "px";
+  bodyEl.appendChild(spanEl);
+  setTimeout(() => {
+    spanEl.remove();
+  }, 3000);
+});
